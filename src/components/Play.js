b7985829467ba4629a1e4fb69ee594581826476c
chrisclarkedev <cclarke8904@gmail.com>
2019-11-22 17:11:49 -0500

    programmed winner condition

--- src/components/Play.js
+++ src/components/Play.js
@@ -16,6 +16,10 @@ export default class Play extends Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+    const winner = calculateWinner(squares);
+    if (winner || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat({
@@ -40,4 +44,29 @@ export default class Play extends Component {
       </div >
     )
   }
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+
+  return null;
 }
